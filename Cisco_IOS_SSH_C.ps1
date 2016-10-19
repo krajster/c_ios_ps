099f4c83c15d8d3bed50c4291c2f2a8da7e7e64f
krajster <krajster@gmail.com>
2016-10-19 18:15:33 +0300

    Create Cisco_IOS_SSH_C.ps1

---
+++ Cisco_IOS_SSH_C.ps1
@@ -0,0 +1,91 @@
+Import-Module SSH-Sessions
+
+### Send commands Cisco IOS
+Function set-SSH($devices, $ssh){
+    function ReadStream($reader)
+    {
+        $line = $reader.ReadLine();
+        while ($line -ne $null)
+        {
+            $line
+            $line = $reader.ReadLine()
+        }
+    }
+
+    function WriteStream($cmd, $writer, $stream)
+    {
+        $writer.WriteLine($cmd)
+        while ($stream.Length -eq 0)
+        {
+            start-sleep -milliseconds 1000
+        }
+    }    
+
+    $stream = $ssh.CreateShellStream("dumb", 80, 24, 800, 600, 1024)
+
+            $reader = new-object System.IO.StreamReader($stream)
+            $writer = new-object System.IO.StreamWriter($stream)
+            $writer.AutoFlush = $true
+
+            while ($stream.Length -eq 0)
+            {
+                start-sleep -milliseconds 500
+            }
+            ReadStream $reader
+
+            foreach ($command in $commands) {
+
+                WriteStream $command $writer $stream
+                ReadStream $reader
+                Write-Host $reader
+                start-sleep -milliseconds 500
+            }
+
+            $stream.Dispose()
+            $ssh.Disconnect()
+            $ssh.Dispose()
+
+}
+
+### Function OpenFile
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv| *.txt"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+### MAIN
+
+### file with Cisco IOS devices
+Write-Host "Enter the path of the file with Cisco devices"
+$filedevices = Get-FileName ".\"
+$devices = Import-Csv -path $filedevices -header 'ip','hostname' -delimiter ';'
+
+### file with commands for Cisco IOS
+Write-Host "Enter the path of the file *.txt with commands Cisco IOS"
+$fileCommands = Get-FileName ".\"
+$commands = Get-Content $fileCommands
+
+### Ask Cisco username password
+$c=get-credential 
+$p=$c.getnetworkcredential().password
+$u=$c.Username
+$user=$u
+$password=$p
+
+$port = 22
+for ($i=0; $i -le $devices.Count; $i++){
+    $ssh = new-object Renci.SshNet.SshClient($devices[$i].ip, $port, $user, $password)
+    Try{
+        $ssh.Connect()
+        Start-Sleep -s 1
+        set-SSH $commands $ssh
+       }
+       catch{
+            }
+}
